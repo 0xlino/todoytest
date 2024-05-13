124ea13de8ac104cfa044b8d8b3b8ecf726d80b5
Lino <13621351+0xlino@users.noreply.github.com>
2024-05-13 17:45:26 +0000

    Add new

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 /**
  * TODO: Second todo
- * Refactor this
+ * Refactor
  */
 async function someFunc(){
+    return true;
+}
+
+/**
+ * TODO: Third todo
+ * Refactor
+ */
+async function someFunc2(){
+    // TODO: Fifth todo
+    // something something
     return true;
 }
